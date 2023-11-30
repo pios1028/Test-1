036948226d0a96bc5e4e8996aca92dbe759fb688
Quoc Huy <piosdaikimiharu@gmail.com>
2023-11-30 10:45:00 +0700

    first commit

---
+++ Ex2/Exercise5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Main Method
+            
+                // Takinga a new input stream i.e.
+                // geeksforgeeks.txt and opens it
+                StreamReader sr = new StreamReader("D://Windows Programming/tmp.txt");
+                Console.WriteLine("Content of the File");
+                // This is use to specify from where
+                // to start reading input stream
+                sr.BaseStream.Seek(0, SeekOrigin.Begin);
+                // To read line from input stream
+                string str = sr.ReadLine();
+                // To read the whole file line by line
+                while (str != null)
+                {
+                    Console.WriteLine(str);
+                    str = sr.ReadLine();
+                }
+                Console.ReadLine();
+                // to close the stream
+                sr.Close();
+            }
+        }
+    }
+
---
+++ Lesson2/Exercise1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating a List of string
+            List<string> mylist = new List<string>();
+            // Inserting elements into List
+            mylist.Add("C#");
+            mylist.Add("Java");
+            mylist.Add("C");
+            mylist.Add("C++");
+            // To get an Enumerator
+            // for the List.
+            List<string>.Enumerator em = mylist.GetEnumerator();
+            //display(em);
+            while (em.MoveNext())
+            {
+                string val = em.Current;
+                Console.WriteLine(val);
+            }
+        }
+    }
+}
