409d670b453dabe2b3b366543262675317a3cc38
Quoc Huy <piosdaikimiharu@gmail.com>
2023-12-21 09:09:36 +0700

    lesson 5

--- Lesson4/Exercise 4/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ Lesson4/Exercise 4/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -61,7 +61,7 @@ namespace Exercise_4 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Exercise 4;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Exercise 4;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
