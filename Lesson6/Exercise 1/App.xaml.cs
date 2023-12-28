dcb7454d7bd406db712d1fffd7c17f4a8a410a86
Quoc Huy <piosdaikimiharu@gmail.com>
2023-12-28 08:10:04 +0700

    Lesson6

---
+++ Lesson6/Exercise 1/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using MVVMInWPF.ViewModel;
+
+namespace Exercise_1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Exercise_1.View.MainPage window = new Exercise_1.View.MainPage();
+            UserViewModel VM = new UserViewModel();
+            window.DataContext = VM;
+            window.Show();
+        }
+    }
+}
