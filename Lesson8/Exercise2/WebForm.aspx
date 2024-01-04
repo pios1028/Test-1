b6278159c7e83f68150b4b21ae15befcfab451c2
Quoc Huy <piosdaikimiharu@gmail.com>
2024-01-04 09:03:03 +0700

    Add lesson 7-9

---
+++ Lesson8/Exercise2/WebForm.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm.aspx.cs" Inherits="Exercise2.WebForm" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<title></title>
+</head>
+    <body>
+        <form id="form1" runat="server">
+            <div>                
+                <asp:Label ID="Label1" runat="server" Text="a"></asp:Label>
+                <asp:TextBox ID="Textbox1" runat="server"></asp:TextBox>
+                <asp:Label ID="Label2" runat="server" Text="b"></asp:Label>
+                <asp:TextBox ID="Textbox2" runat="server"></asp:TextBox>
+            
+                <asp:Button ID="Button1" runat="server" Text="Add" OnClick="Add_Click" style="height: 26px" />
+                <asp:Button ID="Button2" runat="server" Text="Subtract" OnClick="Subtract_Click" style="height: 26px" />
+                <asp:Button ID="Button3" runat="server" Text="Multiply" OnClick="Multiply_Click" style="height: 26px" />
+                <asp:Button ID="Button4" runat="server" Text="Divide" OnClick="Divide_Click" style="height: 26px" />
+            
+                <asp:Label ID="Label3" runat="server" Text=""></asp:Label>
+            </div>
+        </form>
+    </body>
+</html>
---
+++ Lesson8/Exercise2/WebForm.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Exercise2
+{
+    public partial class WebForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Add_Click(object sender, EventArgs e)
+        {
+            WebService webService = new WebService();
+            int a = Convert.ToInt32(Textbox1.Text);
+            int b = Convert.ToInt32(Textbox2.Text);
+            Label3.Text = "Output of WebService: " + webService.Add(a,b).ToString();
+        }
+
+        protected void Subtract_Click(object sender, EventArgs e)
+        {
+            WebService webService = new WebService();
+            int a = Convert.ToInt32(Textbox1.Text);
+            int b = Convert.ToInt32(Textbox2.Text);
+            Label3.Text = "Output of WebService: " + webService.Subtract(a, b).ToString();
+        }
+
+        protected void Multiply_Click(object sender, EventArgs e)
+        {
+            WebService webService = new WebService();
+            int a = Convert.ToInt32(Textbox1.Text);
+            int b = Convert.ToInt32(Textbox2.Text);
+            Label3.Text = "Output of WebService: " + webService.Multiply(a, b).ToString();
+        }
+
+        protected void Divide_Click(object sender, EventArgs e)
+        {
+            WebService webService = new WebService();
+            int a = Convert.ToInt32(Textbox1.Text);
+            int b = Convert.ToInt32(Textbox2.Text);
+            Label3.Text = "Output of WebService: " + webService.Divide(a, b).ToString();
+        }
+    }
+}
